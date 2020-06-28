d8afc10e9517e57932572d231aa1a6f7aea2c08e
David Garvie <garvie88@gmail.com>
2020-06-28 12:23:50 +0100

    refactor: update main file to use new logger module

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const app = require('./src/server');
+const app = require("./src/server");
+const logger = require("./src/logger");
+const { version } = require("./package.json");
 
 const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.info(`Listening on ${PORT}`);
+  logger.info(`version ${version} running on port ${PORT}`);
 });
--- src/server.js
+++ src/server.js
@@ -5,4 +5,3 @@ const app = express();
 app.use("/ping", (req, res) => res.send("pong"));
 
 module.exports = app;
-
