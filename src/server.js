3ec08d694797934ac5428f1ac158d9c96e75bb84
David Garvie <garvie88@gmail.com>
2020-06-28 21:12:24 +0100

    feat(server): send 404 response code for routes without any handlers

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.disable("x-powered-by");
+
 app.use("/ping", (req, res) => res.send("pong"));
+
+app.use((req, res) => {
+  res.status(404).send("Sorry traveler, you seem to have lost your way.");
+});
 
 module.exports = app;
